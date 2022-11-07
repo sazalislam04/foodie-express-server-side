15c46291e4e9182665be6e1fbaff9f1b632022b4
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-07 23:25:29 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 app.use(cors());
 app.use(express());
 
 app.get("/", (req, res) => {
   res.send("Foodie Express server running");
+});
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bts619l.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {

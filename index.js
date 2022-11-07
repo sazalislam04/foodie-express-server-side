441e0fc010c7b9315da6fdf554bd44554423396d
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-08 03:14:09 +0600

    get the services data from db

--- index.js
+++ index.js
@@ -20,6 +20,29 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    console.log("database connect");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+}
+run();
+
+const foodsCollection = client.db("foodie").collection("services");
+
+app.get("/foods", async (req, res) => {
+  try {
+    const query = {};
+    const cursor = foodsCollection.find(query);
+    const result = await cursor.toArray();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Foodie express servier running on port ${port}`);
 });
